95a412d982f33f9964fcae5b69e0bc7859a21b5d
Sara S <ssantmier@gmail.com>
2018-06-25 17:29:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {return Object.assign({}, object, { [key]: value })} 
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {Object[key]= value; return object}
-  object[key] = value;
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {object[key] = value; return object}
+  
 function deleteFromObjectByKey(object, key) {var newObj = Object.assign({}, object);delete newObj.key; return newObj;}
 
 function destructivelyDeleteFromObjectByKey(object, key) {delete Object.key; object}
